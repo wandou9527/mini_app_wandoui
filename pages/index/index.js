9aea993a372764bf01b151a9b97ed35e441629a4
liming <liming@ithro.com>
2019-09-12 18:39:30 +0800

    tabbar

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,135 @@
-//index.js
-//获取应用实例
-const app = getApp()
+//引入本地json数据，这里引入的就是第一步定义的json数据
+var getData = require('../../data/data.js');
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    imgUrls: [],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    toView: 'red',
+    scrollTop: 100,
+    author: 'oreo是我呀',
+    leassonList: [],
+    actIndex: 'first',
+    leassonId: ''
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+  // tab切换事件
+  changeMenu: function (e) {
+    this.setData({
+      actIndex: e.currentTarget.id
+    })
+    if (this.data.actIndex == 'first') {
+      var arr = []
+      for (var i = 0; i < getData.detailList.length; i++) {
+        console.log(getData.detailList[i].leassonType)
+        if (getData.detailList[i].leassonType == 1) {
+          arr.push({
+            id: getData.detailList[i].id,
+            imgUrl: getData.detailList[i].imgUrl,
+            title: getData.detailList[i].title,
+            leassonType: getData.detailList[i].leassonType,
           })
         }
+      }
+      this.setData({
+        leassonList: arr.reverse()
+      })
+    } else if (this.data.actIndex == 'second') {
+      var arr = []
+      for (var i = 0; i < getData.detailList.length; i++) {
+        console.log(getData.detailList[i].leassonType)
+        if (getData.detailList[i].leassonType == 2) {
+          arr.push({
+            id: getData.detailList[i].id,
+            imgUrl: getData.detailList[i].imgUrl,
+            title: getData.detailList[i].title,
+            leassonType: getData.detailList[i].leassonType,
+          })
+        }
+      }
+      this.setData({
+        leassonList: arr.reverse()
+      })
+    } else if (this.data.actIndex == 'third') {
+      var arr = []
+      for (var i = 0; i < getData.detailList.length; i++) {
+        console.log(getData.detailList[i].leassonType)
+        if (getData.detailList[i].leassonType == 3) {
+          arr.push({
+            id: getData.detailList[i].id,
+            imgUrl: getData.detailList[i].imgUrl,
+            title: getData.detailList[i].title,
+            leassonType: getData.detailList[i].leassonType,
+          })
+        }
+      }
+      this.setData({
+        leassonList: arr.reverse()
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  onLoad: function () {
+    console.log(getData)
+    var arr = []
+    for (var i = 0; i < getData.detailList.length; i++) {
+      console.log(getData.detailList[i].leassonType)
+      if (getData.detailList[i].leassonType == 1) {
+        arr.push({
+          id: getData.detailList[i].id,
+          imgUrl: getData.detailList[i].imgUrl,
+          title: getData.detailList[i].title,
+          leassonType: getData.detailList[i].leassonType,
+        })
+      }
+    }
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      imgUrls: getData.bannerList,
+      leassonList: arr.reverse()
     })
+  },
+  //详情页面
+  seeDetail: function (e) {
+    console.log(e)
+    this.setData({
+      leassonId: e.currentTarget.dataset.id
+    })
+    wx.navigateTo({
+      url: '../detail/detail?id=' + this.data.leassonId
+    })
+  },
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: '跟我零基础学彩铅',
+      path: 'pages/index/index'
+    }
   }
 })
